0fc426e35a1f371964c2a953f4f592423cb3951a
Sarah Schwartzel <60547260+sarah-schwartzel@users.noreply.github.com>
2020-02-09 18:27:57 -0500

    Working code to be cleaned

--- BME355_1.py
+++ BME355_1.py
@@ -11,21 +11,21 @@ import matplotlib.pyplot as plt
 import scipy.linalg as la
 
 A = np.array([[0,-1],[1,0]])
-w, v = la.eig(A)
+# w, v = la.eig(A)
 
 
-print(w)
+# print(w)
 # # print(v)
 
 
 # time = np.array()
 
 # state = idek    #init position
-a,b = 1000,1
-alpha = 20000
-p0 = 2000
-g= 9.807
-m=220
+# a,b = 1000,1
+# alpha = 20000
+# p0 = 2000
+# g= 9.807
+# m=220
 
 
 def f(x):
@@ -33,32 +33,40 @@ def f(x):
     :param x: state vector
     :return: derivative of state vector wrt time
     """
-    return np.array([
-        alpha * (x[1] + b*(p0-x[0])/(x[0]+a)),
-        g - x[0]/m,
-        x[1]
-    ])
-
-initState = [1, 0]
-
+    return np.dot(A, x)
+    # return np.array([
+    #     alpha * (x[1] + b*(p0-x[0])/(x[0]+a)),
+    #     g - x[0]/m,
+    #     x[1]
+    # ])
+# def state(currentState, dt, )
+# initState = np.array([1, 0])
+initState = 1
+# currentState = initState
 dt = 0.05
-time = np.arange(0,10,dt)
-x = np.array([0,0,0])
+duration = 10
+time = np.arange(0,duration,dt)
+x = np.array([[0],[0]])
 xVal = []
 for t in time:
-    x = x+dt*f(x)
-    xVal.append(x)
+    if t == 0:
+        x = initState + dt*f(x)
+        xVal.append(x)
+        print(x)
+    else:
+        x = x+dt*f(x)
+        xVal.append(x)
 
 xVal = np.array(xVal)
-plt.subplot(3,1,1)
+# plt.subplot(3,1,1)
 plt.plot(time, xVal[:,0])
-plt.ylabel('Tension (N)')
-plt.subplot(3,1,2)
-plt.plot(time, xVal[:,1])
-plt.ylabel('Velocity (m/s)')
-plt.subplot(3,1,3)
-plt.plot(time, xVal[:,2])
-plt.ylabel('Position (m)')
+plt.ylabel('State')
+# plt.subplot(3,1,2)
+# plt.plot(time, xVal[:,1])
+# plt.ylabel('Velocity (m/s)')
+# plt.subplot(3,1,3)
+# plt.plot(time, xVal[:,2])
+# plt.ylabel('Position (m)')
 plt.xlabel('Time (s)')
 plt.tight_layout()
 # plt.savefig('hill-mass-simulation.png')
