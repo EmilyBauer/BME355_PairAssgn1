3e3f3f17d4891365c0d354b5850f4c0b43c696ce
Emily Bauer <emily.bauer@hellogbye.ca>
2020-02-09 17:11:29 -0500

    wip

--- BME355_1.py
+++ BME355_1.py
@@ -21,29 +21,49 @@ print(w)
 # time = np.array()
 
 # state = idek    #init position
+a,b = 1000,1
+alpha = 20000
+p0 = 2000
+g= 9.807
+m=220
+
+
+def f(x):
+    """
+    :param x: state vector
+    :return: derivative of state vector wrt time
+    """
+    return np.array([
+        alpha * (x[1] + b*(p0-x[0])/(x[0]+a)),
+        g - x[0]/m,
+        x[1]
+    ])
+
+initState = [1, 0]
 
 dt = 0.05
 time = np.arange(0,10,dt)
 x = np.array([0,0,0])
 xVal = []
 for t in time:
-    x += dt*f(x)
+    x = x+dt*f(x)
     xVal.append(x)
 
 xVal = np.array(xVal)
 plt.subplot(3,1,1)
+plt.plot(time, xVal[:,0])
+plt.ylabel('Tension (N)')
+plt.subplot(3,1,2)
+plt.plot(time, xVal[:,1])
+plt.ylabel('Velocity (m/s)')
+plt.subplot(3,1,3)
+plt.plot(time, xVal[:,2])
+plt.ylabel('Position (m)')
+plt.xlabel('Time (s)')
+plt.tight_layout()
+# plt.savefig('hill-mass-simulation.png')
+plt.show()
 
-
-# def f(x):
-#     """
-#     :param x: state vector
-#     :return: derivative of state vector wrt time
-#     """
-#     return np.array([
-#         alpha * (x[1] + b*(p0-x[0])/(x[0]+a)),
-#         g - x[0]/m
-#         x[1]
-#     ])
 # # dt = 0.001
 # x = np.array([0, 0, 0]) #initial condition
 # for i in range(100):
@@ -53,7 +73,11 @@ plt.subplot(3,1,1)
 
 
 # def getFeatures(state):
-
-
+def rk4_update(x):
+    s1 = f(t, x)
+    s2 = f(t + dt/2, x + dt/2*s1)
+    s3 = f(t + dt/2, x + dt/2*s2)
+    s4 = f(t + dt, x + dt*s2)
+    x = x + dt/6*(s1 + 2*s2 + 2*s3 + s4)
 
 
