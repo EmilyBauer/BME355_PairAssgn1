e4d3f77ebc1e4382aef05f6720b7573256a5711c
Emily Bauer <emily.bauer@hellogbye.ca>
2020-02-09 16:24:31 -0500

    updating, adjusting, woooo

--- BME355_1.py
+++ BME355_1.py
@@ -6,6 +6,54 @@ Emily Bauer         20727725
 2020/02/09
 """
 
-print("Eigenvalues")
+import numpy as np
+import matplotlib.pyplot as plt
+import scipy.linalg as la
+
+A = np.array([[0,-1],[1,0]])
+w, v = la.eig(A)
+
+
+print(w)
+# # print(v)
+
+
+# time = np.array()
+
+# state = idek    #init position
+
+dt = 0.05
+time = np.arange(0,10,dt)
+x = np.array([0,0,0])
+xVal = []
+for t in time:
+    x += dt*f(x)
+    xVal.append(x)
+
+xVal = np.array(xVal)
+plt.subplot(3,1,1)
+
+
+# def f(x):
+#     """
+#     :param x: state vector
+#     :return: derivative of state vector wrt time
+#     """
+#     return np.array([
+#         alpha * (x[1] + b*(p0-x[0])/(x[0]+a)),
+#         g - x[0]/m
+#         x[1]
+#     ])
+# # dt = 0.001
+# x = np.array([0, 0, 0]) #initial condition
+# for i in range(100):
+#     x = x+dt*f(x)   #Euler step
+
+# # print(x)
+
+
+# def getFeatures(state):
+
+
 
 
