05d8636624d4c50d9f6cca93e2801a2634a7673a
Sarah Schwartzel <60547260+sarah-schwartzel@users.noreply.github.com>
2020-02-10 22:42:50 -0500

    Finalizing code, adding required comments

--- BME355_1.py
+++ BME355_1.py
@@ -4,6 +4,8 @@ BME355_1.py
 Sarah Schwartzel    20710946
 Emily Bauer         20727725
 2020/02/09
+Note - Please uncomment appropriate lines in order to run desired method.
+(2 lines should be uncommented for any method you want to run)
 """
 
 import numpy as np
@@ -32,11 +34,11 @@ for dt in [dt, 10/dt]:
         time.append(t)
         trajectory.append(x)
         t = t + dt
-        x = x + f(0, x)*dt
-        # x = rk4_update(x)
-        # x = fancyMethod.y
-    plt.plot(times, trajectory)
-    # plt.plot(fancyMethod.t, np.transpose(fancyMethod.y))
+#         x = x + f(0, x)*dt        #Uncomment this code for running the Euler method
+#         x = rk4_update(x)         #Uncomment this code for running the RK4 method
+#         x = fancyMethod.y         #Uncomment this code for running the scipy.integrate.solve_ivp method
+#     plt.plot(times, trajectory)   #Uncomment this plt code for running Euler method or RK4 method
+#     plt.plot(fancyMethod.t, np.transpose(fancyMethod.y)) #Uncomment this code for running the scipy.integrate.solve_ivp method
 plt.xlabel('Time (s)')
 plt.ylabel('State')
 plt.tight_layout()
